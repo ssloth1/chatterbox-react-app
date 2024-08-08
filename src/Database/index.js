0e1c8f702bff931072e9d5353b203d5741dd00a2
ssloth1 <jamesbebarski@gmail.com>
2024-08-08 01:09:35 -0400

    adding Mansi's Code
    it is updated to use ES6 and uses the conventions of ES6 correctly.
    
    if you want to check your connection to the node server from react navigate to /test . just make sure you have your env.local set up. instructions for that are commented in App.tsx
    
    Co-Authored-By: Mansi142000 <155905751+Mansi142000@users.noreply.github.com>

---
+++ src/Database/index.js
@@ -0,0 +1,5 @@
+import topics from './topics';
+import posts from './posts';
+import users from './users';
+
+export { topics, posts, users };
---
+++ src/Topic/index.tsx
@@ -0,0 +1,11 @@
+
+import "./styles.css";
+
+export default function TopicPage() {
+
+	return (
+		<div>
+			<h1>Topic Page</h1>
+		</div>
+	);
+}
